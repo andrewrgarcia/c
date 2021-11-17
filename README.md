bb1c455b89144ffdd5b954c90af35b21a8ef2aaf
andrewrgarcia <andrewrgarcia@ufl.edu>
2021-11-16 22:08:08 -0500

    add args code and explanation in comment of .c and on readme

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 `gcc [program_name].c –o [executable_name]`
 `./[executable_name]`
 
+### Dev: Andrew
+#### args.c
+A simple c code example which shows you how to pass inputs as arguments in terminal with a c program. 
+compiling:
+`gcc args.c -o args`
+deployment:
+`./args [first_word] [second_word] [. . .] [infinite-1_word]`
---
+++ args.c
@@ -0,0 +1,21 @@
+/* A simple c code example which shows you how to pass inputs as arguments in terminal with a c program. 
+   Andrew Garcia, 2021
+*/
+
+#include <stdio.h>
+
+int main( int argc, char *argv[] )  {
+
+   printf("\nprinting all written inputs here:");
+   for(int i=1; i < argc; i++){
+      printf("\n* %s",argv[i]);
+   }
+   printf("\n");
+
+   printf("\nprinting only the first and last written inputs here:");
+   printf("\nyou wrote \"%s\" first",argv[1]);
+   // printf("You wrote %s\n after first",argv[1]);
+   printf("\nyou wrote \"%s\" last",argv[argc-1]);
+   printf("\n");
+
+}
